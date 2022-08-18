2e6fda48ff0e0871cee725c76a2a40430523624b
tamago572 <7f7fnyam@gmail.com>
2022-08-19 03:52:06 +0900

    add file

---
+++ main.py
@@ -0,0 +1,32 @@
+import secrets
+import string
+
+def makePassword(length, isSymbol):
+    # 英数字のみ
+    pass_chars_eisuji = string.ascii_letters + string.digits
+    # 記号を含む
+    pass_chars_eisuji_kigou = string.ascii_letters + string.digits + string.punctuation
+
+    if isSymbol == True:
+        password = ''.join(secrets.choice(pass_chars_eisuji_kigou) for i in range(length))
+    else:
+        password = ''.join(secrets.choice(pass_chars_eisuji) for i in range(length))
+
+    return password
+
+length = input('パスワードの桁数を入力してください>>> ')
+isSymbol = input('記号を含めますか? (y/n)>>> ')
+
+# inputしたlengthがInt型か判定
+if length.isdecimal():
+    print('1以上の整数が入力されました')
+    # 記号を含むか y/nで分岐
+    if 'y' in isSymbol:
+        print("記号を含む" + length + "桁のパスワードを生成します")
+        print(makePassword(int(length), True))
+    else:
+        print("記号を含まない" + length + "桁のパスワードを生成します")
+        print(makePassword(int(length), False))
+
+else:
+    print("1以上の整数を入力してください")
